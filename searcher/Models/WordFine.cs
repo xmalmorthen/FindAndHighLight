283c6a2f85fd86c00c42e68e1821dcc51992a81a
eduardo ruiz <eduruiz07@gmail.com>
2015-12-02 16:31:24 -0600

    Signed-off-by: eduardo ruiz <eduruiz07@gmail.com>

---
+++ searcher/Models/WordFine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace searcher.Models
+{
+    public class WordFine
+    {
+        public string cadenas { get; set; }
+        public int pagina { get; set; }
+        public float coordenada { get; set; }
+    }
+}
